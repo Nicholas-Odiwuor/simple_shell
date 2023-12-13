723b74823a18c9a14bf7fb096b0f0d2ede0a5f1d
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-12-13 15:18:41 -0800

    CLI

---
+++ validagetline.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * validagetline - Executes command lines entered by pipe
+ * @buffer: program name
+ * @len: length of buffer
+ * @args: Argument 1
+ * @env_args: Enviroment arguments
+ * Return: 0
+*/
+void validagetline(char *buffer, int len, char **args, char **env_args)
+{
+	if (len == EOF)
+	{
+		printf("salida EOF\n");
+		write(STDOUT_FILENO, "\n", 1);
+		if (buffer != NULL)
+		{
+			freedom(1, buffer);
+			buffer = NULL;
+		}
+		if (args != NULL)
+		{
+			freedom(2, args);
+			args = NULL;
+		}
+		if (env_args != NULL)
+		{
+			freedom(2, env_args);
+			env_args = NULL;
+		}
+		exit(0);
+	}
+}
+
