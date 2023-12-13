9102d9a0309efa2f42df5bce15296ad04ff518e2
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-12-13 07:00:53 -0800

    Updated shell

---
+++ _itoa.c
@@ -0,0 +1,67 @@
+/**
+ * Nicholas-Odiwuor
+ */
+
+
+#include "main.h"
+/**
+ * lennum - length of number
+ * @n: int
+ * Return: length
+ */
+int lennum(int n)
+{
+	int len = 0;
+
+	while (n / 10 != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+/**
+ * int_to_charac - Converts to string
+ * @num: The integers to be printed
+ * Return: String
+ */
+
+char *int_to_charac(int num)
+{
+	int digit = 0;
+	int i = 0;
+	char *str;
+	int divisor = 1000000000;
+	int len = lennum(num);
+
+	str = malloc(len * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+
+	if (num < 10)
+	{
+		str[i++] = num + '0';
+		str[i] = '\0';
+		return (str);
+	}
+
+	while (divisor)
+	{
+		digit = (num / divisor) % 10;
+		if (digit != 0 || (len >= 0 && str[i - 1] >= '0'))
+		{
+			str[i] = digit + '0';
+			i++;
+			len--;
+		}
+		divisor /= 10;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
+/**
+ * Nicholas-Odiwuor
+ */
