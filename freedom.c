26335278f3a23cd7c469cb4d70196f16248b8e8b
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-12-13 12:47:58 -0800

    Free memory

---
+++ freedom.c
@@ -0,0 +1,67 @@
+#include "main.h"
+/**
+* freedom - Free the memory
+* @n: args
+* Return: null
+*/
+
+void freedom(int n, ...)
+{
+	int i = 0;
+	char **ptr2 = NULL;
+	char *ptr1 = NULL;
+	va_list arg;
+
+	va_start(arg, n);
+
+	if (n == 1)
+	{
+		ptr1 = va_arg(arg, char *);
+		free(ptr1);
+	}
+
+	if (n == 2)
+	{
+		ptr2 = va_arg(arg, char **);
+		while (ptr2[i] != NULL)
+		{
+			free(ptr2[i]);
+			i++;
+		}
+		free(ptr2);
+	}
+}
+
+/**
+* free_all - Free tokenized path directories and user arguments
+* @dptr1: First double pointer to free
+* @dptr2: Second double pointer to free
+* @sptr1: First single pointer to free
+* @sptr2: Second double pointer to free
+* Return: Nothing
+*/
+
+void free_all(char **dptr1, char **dptr2, char *sptr1, char *sptr2)
+{
+
+	if (dptr1 != NULL)
+	{
+		freedom(2, dptr1);
+		dptr1 = NULL;
+	}
+	if (dptr2 != NULL)
+	{
+		freedom(2, dptr2);
+		dptr2 = NULL;
+	}
+	if (sptr1 != NULL)
+	{
+		free(sptr1);
+		sptr1 = NULL;
+	}
+	if (sptr2 != NULL)
+	{
+		free(sptr2);
+		sptr2 = NULL;
+	}
+}
