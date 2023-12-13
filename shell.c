854db438952db2309c80029a3b5f3238132bcaa0
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-12-13 15:42:10 -0800

    Shell

--- shell.c
+++ shell.c
@@ -1,70 +1,29 @@
-#include "shell.h"
-
+#include "main.h"
 /**
- * main - Entry point for the simple shell.
- * 
- * Description: This function runs a basic shell that takes user commands,
- * splits them into arguments, and executes them.
- * 
- * Return: 0 on success, 1 on exit.
- */
-int main(void)
+ * main - Entry point of Shell
+ * @ac: Arguments counter
+ * @av: Argument values
+ * Return: Exit if success = 0
+*/
+
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
-    char *line;
-    char **args;
-    int status;
 
-    do
-    {
-        printf("($) ");
-        line = read_line();
-        args = split_line(line);
-        status = execute_command(args);
+	int int_mode = 1;
+	int errcode = 0;
 
-        free(line);
-        free(args);
 
-    } while (status);
+	errcode = errcode + 0;
 
-    return EXIT_SUCCESS;
+	int_mode = isatty(STDIN_FILENO);
+	if (int_mode == 0)
+	{
+		notty(av);
+	}
+	else
+	{
+
+		_interactive(av);
+	}
+	return (0);
 }
-
-/**
- * read_line - Read a line of input from the user.
- * 
- * Return: A pointer to the read line.
- */
-char *read_line(void)
-{
-    // Implementation of read_line follows Betty style
-    // ...
-
-    return NULL; // Placeholder
-}
-
-/**
- * split_line - Split a line into individual arguments.
- * 
- * Return: An array of strings containing the arguments.
- */
-char **split_line(char *line)
-{
-    // Implementation of split_line follows Betty style
-    // ...
-
-    return NULL; // Placeholder
-}
-
-/**
- * execute_command - Execute a command with its arguments.
- * 
- * Return: 0 on success, 1 on exit.
- */
-int execute_command(char **args)
-{
-    // Implementation of execute_command follows Betty style
-    // ...
-
-    return 0; // Placeholder
-}
-
